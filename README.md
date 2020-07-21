88801a6eb217e701f3e5378ed41ab5e5c19620e8
Justice <justice.addico.ja@gmail.com>
2020-07-21 13:47:40 +0000

    Modified Readme

--- README.md
+++ README.md
@@ -5,14 +5,43 @@ A package that allows you tooltip to show intelligently base on where it is
 
 `npm i smart-tooltip --save`
 
-Then ...
+`optionally run npm init -y`
+
+# Bundling your app
+
+Html (index.html)
 
 ```
-    import {smartTooltip} from "./index.js";
+<head>
+    <link rel="stylesheet" href="node_modules/smart-tooltip/main.css">
+</head>
+<body>
+    <div class="smart-tooltip-container btn1" > 
+                Button 
+        <span class="smart-tooltip">Welcome</span>  
+    </div>
+</body>
+<script src="yourscript.js"></script>
+```
+
+Javascript (yourscript.js)
+
+```
+    import {smartTooltip} from "smart-tooltip";
 
     smartTooltip();
 ```
 
+Bundling with Parcel (optional)
+
+```
+    run npm i parcel -g
+
+    parcel index.html
+
+```
+
 ## options
-smart-tooltip supports 2 options all of which are optional
-* *type* - _hard | soft_ (Defaults to soft)
+* no options yes
+<!-- smart-tooltip supports 2 options all of which are optional
+* *type * - _hard | soft_ (Defaults to soft) -->
--- index.js
+++ index.js
@@ -1,71 +1,77 @@
 
-function smartTooltip(){
-    console.log("banku");
+export const smartTooltip = function smartTooltip() {
     var btnArray = document.querySelectorAll(".smart-tooltip-container");
     var body = document.querySelector("body");
-    
+
     btnArray.forEach(element => {
-        element.addEventListener("mouseover", myfunc);
+        if (myfunc)
+            element.addEventListener("mouseover", myfunc);
     });
 
     function myfunc(event) {
         if (!event)
             return;
-    
+
         let btn = event.target;
-        let tooltip = btn.children[0];
+        if (!btn) return;
+
+        let tooltip = btn.querySelector(".smart-tooltip");
         
+        if(!tooltip)return;
+
+
+
         var tooltipclient = tooltip.getBoundingClientRect();
         var btnclient = btn.getBoundingClientRect();
         var bodyclient = body.getClientRects()[0];
-    
-    
-        if (btnclient.left > tooltipclient.width && bodyclient.height > tooltipclient.height) {
-    
+
+
+        if (btnclient.left > tooltipclient.width) {
+
             let topHalf = (btnclient.bottom - (btnclient.height / 2));
             let bottomHalf = bodyclient.height - topHalf;
             let halfTooltip = (tooltipclient.height / 2);
-    
+
             if ((topHalf > halfTooltip) && (bottomHalf > halfTooltip)) {
                 tooltip.classList.remove(tooltip.classList[1]);
                 tooltip.classList.add('left');
-    
+
             } else if (topHalf < halfTooltip) {
                 tooltip.classList.remove(tooltip.classList[1]);
                 tooltip.classList.add('left-t');
-    
+
             } else if (bottomHalf < halfTooltip) {
                 tooltip.classList.remove(tooltip.classList[1]);
                 tooltip.classList.add('left-b');
-    
+
             }
-    
-        } else if ((bodyclient.width - btnclient.right) > tooltipclient.width && bodyclient.height > tooltipclient.height) {
-    
+
+        } else if ((bodyclient.width - btnclient.right) > tooltipclient.width) {
+
             let topHalf = (btnclient.bottom - (btnclient.height / 2));
             let bottomHalf = bodyclient.height - topHalf;
             let halfTooltip = (tooltipclient.height / 2);
-    
-    
+
+
             if ((topHalf > halfTooltip) && (bottomHalf > halfTooltip)) {
                 tooltip.classList.remove(tooltip.classList[1]);
                 tooltip.classList.add('right');
             } else if (topHalf < halfTooltip) {
                 tooltip.classList.remove(tooltip.classList[1]);
                 tooltip.classList.add('right-t');
-    
-    
+
+
             } else if (bottomHalf < halfTooltip) {
                 tooltip.classList.remove(tooltip.classList[1]);
                 tooltip.classList.add('right-b');
-    
-    
+
+
             }
-    
-        } else if (btnclient.top > tooltipclient.height && (bodyclient.width - btnclient.bottom) < tooltipclient.height) {
+
+        } else if (btnclient.top > tooltipclient.height) {
             tooltip.classList.remove(tooltip.classList[1]);
             tooltip.classList.add('top');
-        } else if (bodyclient.height - btnclient.bottom > tooltipclient.height) {
+        } else if ((bodyclient.height - btnclient.bottom) > tooltipclient.height) {
             if (btnclient.left > (bodyclient.width - btnclient.right) || btnclient.left > btnclient.bottom || btnclient.left > btnclient.top) {
                 tooltip.classList.remove(tooltip.classList[1]);
                 tooltip.classList.add('bottom');
@@ -73,10 +79,13 @@ function smartTooltip(){
                 tooltip.classList.remove(tooltip.classList[1]);
                 tooltip.classList.add('bottom');
             }
+        }else{
+            tooltip.classList.remove(tooltip.classList[1]);
+            tooltip.classList.add('bottom');
         }
-    
+
     }
 }
 
-module.exports.smartTooltip = smartTooltip;
+//module.exports.smartTooltip = smartTooltip;
 
